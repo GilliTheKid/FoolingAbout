6a1ba67be3403cc3741f5d04b2bbb492c690bab3
Andrew Gillibrand (ADRGLT) <andrew@upstream.info>
2011-05-19 20:39:16 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoolingAbout::Application.config.secret_token = '7565fffb8f044e80fb038c666e70e3e2053b531089a1b431802dc903082ece79938e3e2dd0b9b419b6fc0a7bb76b97141f414fe092ea1684889aaf684c77b993'
